997377b2508b34d16638897560f6b367abea9c45
Sankalp Kulshreshtha <skulshre@andrew.cmu.edu>
2013-04-26 14:19:33 -0400

    Get request for prototypes working

---
+++ static/js/prototypeView.js
@@ -0,0 +1,26 @@
+/* AJAX */
+console.log("hello");
+
+var prototype;
+$(document).ready(function(){
+	getPrototype(getPrototypeId());
+});
+
+/* gets the prototypes id from the url */
+function getPrototypeId(){
+	var path = window.location.pathname;
+	var index = path.lastIndexOf("/");
+	var id = path.slice(index+1);
+	return id;
+}
+
+/* get the prototype from the server based on id */
+function getPrototype(id){
+  $.ajax({
+    type: "get",
+    url: "/prototypes/"+id,
+    success: function(data) {
+      console.log(data);
+    }
+  });
+}
---
+++ static/js/setup.js
@@ -0,0 +1,3 @@
+var RAMEN_PATH = {
+	"server"	: 'http://128.237.235.214:3000',
+};
